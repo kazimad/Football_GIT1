fced7d96c86d1ebf5d866533e0583b265e167662
kazimad <kazimad.kr@gmail.com>
2014-01-06 15:15:35 +0200

    create

---
+++ comand.java
@@ -0,0 +1,39 @@
+package Football_GIT1;
+
+/**
+ * Created by KaZimad on 06.01.14.
+ */
+public class comand {
+
+
+    public class Comand {
+        private String name;
+        private int games;
+        private int goals;
+        private int fails;
+        private int scores;
+
+        public Comand (String name, int games,int goals, int fails, int scores) {
+            this.name = name;
+            this.games = games;
+            this.goals = goals;
+            this.fails = fails;
+            this.scores = scores;
+        }
+        public String getName (){
+            return name;
+        }
+        public int getGames(){
+            return games;
+        }
+        public int getGoals(){
+            return goals;
+        }
+        public int getFails(){
+            return fails;
+        }
+        public int getScores(){
+            return scores;
+        }
+    }
+}
---
+++ desk.java
@@ -0,0 +1,12 @@
+package Football_GIT1;
+
+/**
+ * Created by KaZimad on 06.01.14.
+ */
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+public class desk {
+
+}
