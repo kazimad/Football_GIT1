446efa7f4dfd1f6f8bb3ae440befd9aad7e81eb8
kazimad <kazimad.kr@gmail.com>
2014-01-29 11:07:23 +0200

    rewrite

--- desk.java
+++ desk.java
@@ -14,28 +14,74 @@ public class Desk extends JFrame implements ActionListener {
     static JTable myTable;
     static JFrame frame;
     static JScrollPane jscrlpn;
+    static JButton button1,button2,button3,button4;
+    static JPanel myPanel1, myPanel2, mainPanel,panelData;
     static JLabel label;
 
     public void desk(){
         frame = new JFrame("Чемпионат по футболу"); // ожидается что это будет родительское окно, в котором будут вкладки
         frame.setTitle("Чемпионат по футболу");
-        frame.getContentPane().setLayout(new FlowLayout());
-        frame.setSize(300,170);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        myPanel1 = new JPanel();
+        myPanel2 = new JPanel();
+        mainPanel = new JPanel();
+        panelData = new JPanel();
+        label = new JLabel(" Информация о команде");
         myTable = new JTable(FirstWindow.means,FirstWindow.heads);
         jscrlpn = new JScrollPane(myTable);
-        myTable.setPreferredScrollableViewportSize(new Dimension(250,150));  // размеры окна прокруутки
-        frame.getContentPane().add(jscrlpn);
 
-        frame.pack();  //  !!!! добавить
-        frame.setLocation(300, 300);
+        components_myPanel1();
+        components_myPanel2();
+
+
+
+        panelData.setLayout(new BorderLayout());
+        panelData.add(label, BorderLayout.NORTH);
+
+        panelData.add(myPanel2,BorderLayout.CENTER);
+
+
+
+        mainPanel.add(myPanel1);
+        mainPanel.add(panelData); 
+        frame.getContentPane().add(mainPanel);
+
+
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocation(300, 100);
+
+
+        frame.setPreferredSize(new Dimension(250, 200));  // размеры окна прокруутки
+        frame.setResizable(false);
+
+
+
+        //frame.pack();  //  !!!! добавить
         frame.setVisible(true);
 
-}
+    }
+    private void  components_myPanel1(){
+        button1 = new JButton("Информация о команде");
+        button2 = new JButton("Таблица");
+        button3 = new JButton("Результаты");
+        button4 = new JButton("Сохранить в файл");
+        myPanel1.setLayout(new FlowLayout());
+        myPanel1.add(button1);
+        myPanel1.add(button2);
+        myPanel1.add(button3);
+        myPanel1.add(button4);
+    }
 
+    private void components_myPanel2(){
+
+        myPanel2.add(jscrlpn);
+        myPanel2.setLayout(new GridLayout(1,4));
+    }
     @Override
     public void actionPerformed (ActionEvent e){}
 
 }
 
 
+//
